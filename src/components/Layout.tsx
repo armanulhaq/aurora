90768ade841b840875ef1480ac1944d2c54a76ef
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-11-26 15:33:24 +0530

    add comments and minor changes to footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,11 +6,19 @@ const layout = ({ children }: PropsWithChildren) => {
         <div className="bg-gradient-to-br from-background to-muted">
             <Header />
             <main className="min-h-screen container mx-auto px-4 py-8">
+                {" "}
+                {/* min-h-screen sets the minimum height to 100vh, but it starts calculating
+                    from where the element is positioned, not from the top of the viewport
+                    container: Makes the element follow container width constraints
+                    mx-auto: Centers the container horizontally using auto margins
+                    px-4: Adds horizontal padding of 1rem (16px)
+                    py-8: Adds vertical padding of 2rem (32px)
+                */}
                 {children}
             </main>
             <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
                 <div className="container mx-auto px-4 text-center text-gray-400">
-                    Made with love
+                    Made with ❤️ by Arman
                 </div>
             </footer>
         </div>
